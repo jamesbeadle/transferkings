7843a8c9a7af07744b0c5aeaf28022f1cc6dbff2
James Beadle <jamesbeadle1989@gmail.com>
2024-06-16 17:25:46 +0100

    Set connect / play based on login state

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -1,7 +1,16 @@
-<script>
-    import LogoIcon from "$lib/icons/logo-icon.svelte";
+<script lang="ts">
+    import { authSignedInStore } from "$lib/derived/auth.derived";
+import LogoIcon from "$lib/icons/logo-icon.svelte";
+    import { authStore, type AuthSignInParams } from "$lib/stores/auth-store";
 import Layout from "./Layout.svelte";
 
+
+    function handleLogin() {
+        let params: AuthSignInParams = {
+        domain: import.meta.env.VITE_AUTH_PROVIDER_URL,
+        };
+        authStore.signIn(params);
+    }
 </script>
 <Layout>    
     <div class="p-4">
@@ -13,8 +22,12 @@ import Layout from "./Layout.svelte";
         <a href="/rules">
             <button class="bg-Brand1b my-2 px-4 py-2 rounded-sm">Rules</button>
         </a>
-        <a href="/contract-center">
-            <button class="bg-gray-500 my-2 px-4 py-2 rounded-sm">Play</button>
-        </a>
+            {#if $authSignedInStore}
+                <a href="/contract-center">
+                    <button class="bg-gray-500 my-2 px-4 py-2 rounded-sm">Play</button>
+                </a>
+            {:else}
+                <button class="bg-gray-500 my-2 px-4 py-2 rounded-sm" on:click={handleLogin}>Connect</button>
+            {/if}
     </div>
 </Layout>
